b2a3cb189d3f4c6cd6c8d8da3a291e7a13a3bffa
SOuellette22 <ouellettese@merrimack.edu>
2026-03-18 15:47:49 -0400

    feat: added the necessary Pydantic schemas that allow for users to be created seen through a public view (no email) or private view (email) or for users to login.

---
+++ backend/app/schemas/users.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    image_file: str | None
+    image_path: str
+
+class UserPrivate(UserPublic):
+    email: EmailStr
+
+class UserLogin(UserBase):
+    # TODO Implement the UserLogin pydantic model
+    pass
