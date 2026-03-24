bd5f807a67cba44ac176be5273a7bff82ec54941
SOuellette22 <ouellettese@merrimack.edu>
2026-03-24 15:33:51 -0400

    Fix: forgot to add the changes to the main.py file that make the friends logic testable.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 from backend.app.database import Base, engine
 from backend.app.routers import (
-    users
+    users,
+    friends
 )
 from backend.app.models import friends as _friends_model
 
@@ -12,6 +13,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="WatchTogether API") # Initializes the app
 
 app.include_router(users.router)
+app.include_router(friends.router)
 
 @app.get("/")
 def root():
